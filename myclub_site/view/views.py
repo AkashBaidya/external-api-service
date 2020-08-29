df5f942847f0408de2d33287fbf8365c8d1641dd
Akash Baidya <akashbaidya2@hotmail.com>
2020-08-29 08:06:41 -0700

    final update

--- myclub_site/view/views.py
+++ myclub_site/view/views.py
@@ -19,34 +19,7 @@ from rest_framework.permissions import IsAuthenticated
 
 def dashboard_view(request,string):
     permission_classes = (IsAuthenticated, )
-    #print(request.POST)
-
-
-
-    # received_json_username=json.loads(request.POST['username'])
-    # received_json_password=json.loads(request.POST['password'])
-    # print(received_json_username,received_json_password)
-
-    # user = authenticate(username=received_json_username, password=received_json_password)
-    # if user is not None:
-    #     data = {'name': 'Vitor',
-    #             'location': 'Finland',
-    #             'is_active': True,
-    #             'count': 28
-    #             }
-    #     return JsonResponse(data)
-    #     # A backend authenticated the credentials
-    # else:
-    #     print("kello")
-    #     data = {'name': 'Vitor',
-    #             'location': 'Finland',
-    #             'is_active': True,
-    #             'count': 28
-    #             }
-    #     return JsonResponse(data)
-        # No backend authenticated the credentials
-    # url = "http://192.168.107.11/account/"+string
-    # print(url)
+    
 
     url = "https://192.168.107.11:8585/auth"
     headers = {'content-type': 'application/json'}
@@ -60,18 +33,12 @@ def dashboard_view(request,string):
     #print(decoded_hand['access_token'])
 
 
-    #
+    
     headers_final={
     'authorization': 'JWT '+decoded_hand['access_token']
     }
-    print(headers_final)
-    # #
-    # print(headers_final)
+    
     url = "https://192.168.107.11:8585/account/"+string
     r_final=requests.get(url,headers=headers_final)
-    #print(requests.get(url,headers=headers_final))
-    #print(url)
-
-    # r=requests.post(url, data=json.dumps(data), headers=headers)
-    # r=requests.get(url, headers=headers)
+    
     return HttpResponse(r_final.text,content_type='application/json')
